6afe4060c6477dee28c4969bca9eb7fcfc46c5c3
Your name <val.sytch@gmail.com>
2016-08-13 17:54:37 +0300

    .

--- Decorator/decorator_greeting.py
+++ Decorator/decorator_greeting.py
@@ -8,9 +8,9 @@ def say_good_luck(greeting_with_you):
     def good_luck(name):
         if name is not None:
             greeting_with_you(name)
-            print ("Nice to meet you and good luck, " + name.capitalize())
+            print("Nice to meet you and good luck, " + name.capitalize())
         else:
-            print ("Oops, it looks like you do not enter your name")
+            print("Oops, it looks like you do not enter your name")
     return good_luck
 
 
